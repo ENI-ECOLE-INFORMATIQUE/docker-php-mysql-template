c5d98227418f00ceca1c9470813020b2c41a13d6
Denis Sanchez <dsanchez@campus-eni.fr>
2026-05-20 16:14:53 +0200

    changement init sql

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,14 +1,3 @@
-CREATE TABLE utilisateurs (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(100)
-);
-
-INSERT INTO utilisateurs (nom)
-VALUES
-('Alice'),
-('Bob'),
-('Charlie');
-
 CREATE DATABASE IF NOT EXISTS legumes
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
