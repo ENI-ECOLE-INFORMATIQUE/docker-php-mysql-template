e2c4b66b7937f1b5ad28ab3802b223a17d953582
Denis Sanchez <dsanchez@campus-eni.fr>
2026-05-20 14:27:43 +0200

    Ajout du coursPHP mise à jour de la BD et creation de BDlegumes ainsi que de l'utilisateur

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,15 @@
 FROM php:8.3-apache
+RUN apt-get update && apt-get install -y \
+    libzip-dev \
+    zip \
+    unzip
 
-RUN docker-php-ext-install mysqli pdo pdo_mysql
+RUN docker-php-ext-install \
+    mysqli \
+    pdo \
+    pdo_mysql \
+    zip \
+    gd
 
 RUN a2enmod rewrite
 
--- mysql/init.sql
+++ mysql/init.sql
@@ -8,3 +8,16 @@ VALUES
 ('Alice'),
 ('Bob'),
 ('Charlie');
+
+CREATE DATABASE IF NOT EXISTS legumes
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+CREATE USER IF NOT EXISTS 'phpUser'@'%'
+IDENTIFIED BY 'NIMva2m95dRnLawt';
+
+GRANT ALL PRIVILEGES
+ON legumes.*
+TO 'phpUser'@'%';
+
+FLUSH PRIVILEGES;
